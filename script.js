dd2645aaefeecf8fc5dfb69a6ff7ddf0a98f63d7
Aapple21 <aapplegate12@icloud.com>
2022-11-02 19:19:12 -0500

    Second Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// document.getElementById('circleValue').addEventListener('change', ({ target }) => {
+//     let { value: circleValue }= target;
+//     if (circleValue > 100) {
+//       circleValue = 100;
+//     }
+//     let successValue = 100;
+//     document.getElementById('circle-percentage').innerHTML= `${circleValue}%`;
+//     document.getElementById('success-value').setAttribute('stroke-dasharray', `${successValue}, 660`);
+//   });
+
+
+
+
